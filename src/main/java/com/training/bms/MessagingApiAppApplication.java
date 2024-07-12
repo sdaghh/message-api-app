a1727b7f6793a20325ff0d8b9c3275ec4697a60f
sdaghh <chuvvi4brunda@gmail.com>
2024-07-12 15:05:12 +0530

    new changes

--- src/main/java/com/training/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training/bms/MessagingApiAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MessagingApiAppApplication {
 	public void printGreeting()
 	{
-		System.out.println("hello oracle");
+		System.out.println("brunda");
 	}
 
 	public static void main(String[] args) {
